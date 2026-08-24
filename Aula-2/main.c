19d0d0dd489bae141458e94141d279fc2ff0e936
ryanbarbosahoracio926-gif <ryanbarbosahoracio926@gmail.com>
2026-08-24 20:18:34 -0300

    Add files via upload

---
+++ Aula-2/main.c
@@ -0,0 +1,22 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int idade;
+    
+    printf("Digite Sua Idade");
+    
+    scanf("%d", &idade);
+    
+    printf("Sua Idade é %d anos ", idade);
+
+    return 0;
+}
