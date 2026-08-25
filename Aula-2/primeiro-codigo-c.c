26168161495ad86607249840c5ada5a2ffcdd71f
Ryan <ryanbarbosahoracio926@gmail.com>
2026-08-25 13:01:19 -0300

    Add files via upload

---
+++ Aula-2/primeiro-codigo-c.c
@@ -0,0 +1,22 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+#define PI 3.14159
+
+ int main(void) {
+    float raio, area;
+    printf("3.14159: ");
+    scanf("%f", &raio);
+    ("area= PI * raio * raio");
+    printf("Àrea = %.2f/n", area);
+    
+    return 0;
+    
+    
+}
