e85773dda3d93542a3c183e9a733b599ceb8644e
ryanbarbosahoracio926-gif <ryanbarbosahoracio926@gmail.com>
2026-08-24 21:37:26 -0300

    Add files via upload

---
+++ Aula-2/exemplo-aplicaçao-01.c
@@ -0,0 +1,27 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int rgm = 12345;
+    
+    float media = 8.758;
+    
+    printf("Dados do aluno:Ryan Barbosa Horacio \n");
+    
+    printf("RGM: %d\n", rgm);
+    
+    printf("Média: %.2f\n", media);
+    
+    
+   
+
+    return 0;
+}
